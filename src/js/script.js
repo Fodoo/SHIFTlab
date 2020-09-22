d13800385bc778c07723ba546b1130be8e17a1a3
Dmitriy <d29bondarenko@gmail.com>
2020-09-23 04:41:58 +0700

    Переделана структура HTML/CSS, написан интерактив на JS, сделаны полностью оба задания

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,36 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+    const inputForm = document.querySelector(".table__input"),
+        btnForm = document.querySelector(".table__btn"),
+        column = document.querySelectorAll("[data-search]"),
+        textUnderForm = document.querySelector(".table__text");
+
+    search();
+
+    function search() {
+        btnForm.addEventListener("click", (e) => {
+            e.preventDefault();
+            const reg = new RegExp(inputForm.value, "ig");
+            let count = 0;
+
+            if (inputForm.value != 0) {
+                column.forEach((item) => {
+                    item.style.backgroundColor = "#ffffff";
+                    item.style.color = "#000000";
+                    if (item.textContent.match(reg)) {
+                        item.style.backgroundColor = "red";
+                        item.style.color = "#ffffff";
+                        count++;
+                    }
+                });
+            }
+
+            if (count == 0) {
+                textUnderForm.textContent = "Ничего не найдено";
+            } else {
+                textUnderForm.textContent = "Найдено: " + count;
+            }
+        });
+    }
+});
